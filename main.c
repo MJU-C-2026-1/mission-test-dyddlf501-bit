02af5a07d300891de1d21d02e3ba8e0937cead0f
kwonyongil <dyddlf501@mju.ac.kr>
2026-03-20 21:20:28 +0900

    "Hello World 작성 완료"
    mission1

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 /*
-  파일이름:
-  작 성 자:
+  파일이름: Hello World
+  작 성 자: 권용일 2026-03-20
 */
+
+#include<stdio.h>    //standard input/output
+
+int main()    //main 함수
+{
+  printf("Hello, World"\\n");    /출력 함수
+  printf("First Line\n Second Line\n");
+  printf("권용일\t 23\t 증평\n");
+  
+  return 0;
+}
