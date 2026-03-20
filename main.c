8f338a0b4247cd00690679bb3695da726b7d2832
kwonyongil <dyddlf501@mju.ac.kr>
2026-03-20 21:24:08 +0900

    "Hello World 작성 완료"

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 */
 
 #include<stdio.h>    //standard input/output
-
 int main()    //main 함수
 {
-  printf("Hello, World"\\n");    /출력 함수
+  printf("Hello, World"\\n");    //출력 함수
   printf("First Line\n Second Line\n");
   printf("권용일\t 23\t 증평\n");
   
