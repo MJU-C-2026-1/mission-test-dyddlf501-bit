fafa8b987dbc4d8891f21aa00023f364a2097016
kwonyongil <dyddlf501@mju.ac.kr>
2026-03-20 21:28:03 +0900

    "Hello World 작성완료"

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
   작 성 자: 권용일 2026-03-20
 */
 
-#include<stdio.h>    //standard input/output
-int main()    //main 함수
+#include<stdio.h>    
+int main()    
 {
-  printf("Hello, World"\\n");    //출력 함수
-  printf("First Line\n Second Line\n");
-  printf("권용일\t 23\t 증평\n");
+  printf("Hello, World"\n");
+  printf("First Line\nSecond Line\n");
+  printf("권용일\t23\t증평\n");
   
   return 0;
 }
